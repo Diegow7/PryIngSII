2c45ac6e8c0e903d7e1fc11c2e0fd296c48af14f
diegow7 <diegorafaelariassarango7@gmail.com>
2023-11-13 15:14:00 -0500

    Se modifico el mensaje del main 2.0

--- src/Paquete/Main.java
+++ src/Paquete/Main.java
@@ -17,7 +17,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        System.out.println("Mensaje de prueba again");
+        System.out.println("Mensaje de prueba again 2.0");
         //Llamado a ventana principal
         VentanaMain ventanaMain = new VentanaMain();
         ventanaMain.setVisible(true); 
