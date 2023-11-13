e427aeec462240a6034a02aa84f47a1963d308ce
Diegow7 <diegorafaelariassarango7@gmail.com>
2023-11-12 21:46:32 -0500

    Se agrega el proyecto

---
+++ src/Paquete/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Paquete;
+
+import Vista.VentanaMain;
+
+/**
+ *
+ * @author diego
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Llamado a ventana principal
+        VentanaMain ventanaMain = new VentanaMain();
+        ventanaMain.setVisible(true);
+    }
+    
+}
